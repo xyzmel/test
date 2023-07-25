88bae8d32cad15ea7e8b7293589b5fea10693708
kjetil melby <kjetil.melby123@gmail.com>
2023-07-25 19:25:41 +0200

    Bug fixes

--- sections/Hero.js
+++ sections/Hero.js
@@ -1,10 +1,8 @@
 import { home } from "@/assets/data/dummydata"
 import Banner from "@/components/Banner"
 import Expertise from "@/components/Expertise"
-import ShowCase from "@/components/ShowCase"
 import Testimonial from "@/components/Testimonial"
 import { Title, TitleLogo, TitleSm } from "@/components/common/Title"
-import { BlogCard, Brand } from "@/components/router"
 import React from "react"
 
 const Hero = () => {
